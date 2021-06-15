e2b653609ca11359560984ebf470529e0c3cded9
Roshan Rane <roshan.rane97@gmail.com>
2021-06-16 00:37:57 +0530

    + logs when folder creation failed

--- utils/tools.go
+++ utils/tools.go
@@ -10,6 +10,7 @@ func CreateFolder(folderName string) error {
         if _, err := os.Stat(folderName); os.IsNotExist(err) {
             err := os.Mkdir(folderName, 0755)
             if err != nil {
+                log.Println("Failed to create directory", folderName)
                 return err
             } else {
                 log.Println("Created folder", folderName)
